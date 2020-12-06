3c82ffd78b23e79fc8b7c65435a00d638be2da30
yinkaenoch <adedokunyinkaenoch@gmail.com>
2020-12-06 22:00:26 +0100

    [feat] add profile page

--- aepp/src/UI/ProfilePage.js
+++ aepp/src/UI/ProfilePage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+import logo from "../assets/logo-small.png";
+import styles from "./ProfilePage.module.scss";
 
-const ProfilePage = () => <h1>ProfilePage</h1>;
+const ProfilePage = ({ profileImg, username, address, about }) => {
+	return (
+		<section className={styles.container}>
+			<figure className={styles.profileImg}>
+				<img src={logo} alt="" />
+			</figure>
+
+			<div className={styles.info}>
+				<span>Name</span>
+				<p>Yinka</p>
+
+				<span>Address</span>
+				<p>ak_23</p>
+
+				<span>About</span>
+				<p>Cool guy</p>
+			</div>
+		</section>
+	);
+};
 
 export default ProfilePage;
+
+// const mapStateToProps = (state) => ({ userProfile: state.userProfile });
+
+// export default connect(mapStateToProps, null)(ProfilePage);*/
--- aepp/src/UI/SearchPage.js
+++ aepp/src/UI/SearchPage.js
@@ -1,5 +1,27 @@
 import React from "react";
+import styles from "./SearchPage.module.scss";
 
-const SearchPage = () => <h1>SearchPage</h1>;
+const SearchPage = () => {
+	const runQuery = (query) => {
+		console.log(query);
+	};
+
+	return (
+		<section className={`container ${styles.container}`}>
+			<header className={styles.header}>
+				<input
+					type="search"
+					name="query"
+					id="query"
+					className={styles.input}
+					placeholder="Search..."
+					onChange={(e) => runQuery(e.target.value)}
+				/>
+			</header>
+
+			<div></div>
+		</section>
+	);
+};
 
 export default SearchPage;
