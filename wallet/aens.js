e2363fc7ee5f2bf1c24a1c70ebf44e54c9590b0d
Emmanuel Joseph(JET) <emmanueljet774@gmail.com>
2020-12-23 16:04:16 +0100

    package.json & aens.js update
    Changes to be committed:
    	modified:   package.json
    	modified:   wallet/aens.js

--- wallet/aens.js
+++ wallet/aens.js
@@ -1,8 +1,7 @@
-const { default: config } = require('./config')
+const config = require('./config')
 const { Universal: Ae, MemoryAccount, Node } = require('@aeternity/aepp-sdk')
 
 const main = async (name) => {
-
   const node = await Node({ url: config.url, internalUrl: config.internalUrl })
   const acc = MemoryAccount({
     keypair: {
@@ -20,13 +19,17 @@ const main = async (name) => {
     ],
   })
 
-  const preclaim = await client.aensPreclaim(name)
-  console.log(preclaim)
-  const claim = await client.aensClaim(name, preclaim.salt)
-  console.log(claim)
-  const update = await client.aensUpdate(name, [config.publicKey])
-  console.log(update)
-  
+  await client.aensPreclaim(name).then((preclaim) => {
+    console.log("AENS Preclaim", preclaim);
+
+    await client.aensClaim(name, preclaim.salt).then((result) => console.log("AENS Claim", result)).catch((e) => console.error("AENS Claim", e)) 
+  }).catch((e) => console.error("AENS Preclaim", e))
+
+  await client.aensQuery(name).then((result) => console.log("AENS Query", result)).catch((e) => console.error("AENS Query", e))
+
+  await client.aensUpdate(name, [config.publicKey]).then((result) => console.log("AENS Update", result)).catch((e) => console.error("AENS Update", e))
+
+  await client.aensTransfer(name, config.recipientPub).then((result) => console.log("AENS Transfer", result)).catch((e) => console.error("AENS Transfer", e))
 }
 
 main(config.name)
